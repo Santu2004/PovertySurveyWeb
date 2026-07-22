230faec57ab625d5af9f48a1475b4f05187a4900
Santu2004 <santoshchapadi589@gmail.com>
2026-07-23 01:00:26 +0530

    changing of url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://povertysurveyweb-1.onrender.com/api",
+  baseURL: "https://povertysurveyweb-2.onrender.com/api",
 });
 
 export default api;
